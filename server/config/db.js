88e9ba7cb37f4d504a7c188d6e2901e43deea8b5
cxlos <bautistac_29@outlook.com>
2023-06-06 19:31:25 -0500

    added URI

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/test', {
+mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/bird-brain', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
