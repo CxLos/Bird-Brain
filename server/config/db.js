6572fda7cbd29a55069e74afa3f9e3d52d19ef7c
cxlos <bautistac_29@outlook.com>
2023-06-07 17:04:49 -0500

    changed db file

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 // mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/bird-brain', {
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bird-brain', {
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bird-brain', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/bird-brain', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
