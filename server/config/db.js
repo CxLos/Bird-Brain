129b113c451e91e99fa3da0f7b80e830e03ad314
cxlos <bautistac_29@outlook.com>
2024-04-15 22:54:01 -0500

    tried adjusting IP address

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,8 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/bird-brain', {
-// mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/bird-brain', {
-// mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/bird-brain', {
-// mongoose.connect(process.env.MONGO_URI || 'mongodb://0.0.0.0:27017/bird-brain', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bird-brain', {
 // mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/bird-brain', {
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/bird-brain', {
 
   useNewUrlParser: true,
   useUnifiedTopology: true,
