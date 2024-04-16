05477614c1b9452a8a418efb9fcb016f325b313f
cxlos <bautistac_29@outlook.com>
2024-04-16 12:56:14 -0500

    played around with mongo connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-const uri = 'mongodb://cxlos:c6uinQzP6UjGcwS2:27017/bird-brain';
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://cxlos:c6uinQzP6UjGcwS2@cluster0.6bz6oix.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = process.env.MONGODB_URI;
+const uri2 = process.env.MONGO_URI;
 
 // const options = {
 //     useNewUrlParser: true,
@@ -20,11 +23,43 @@ const uri = 'mongodb://cxlos:c6uinQzP6UjGcwS2:27017/bird-brain';
 //   })
 // }
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// async function run() {
+//   try {
+
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bird-brain', {
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://0.0.0.0/0/bird-brain', {
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://136.49.66.51/bird-brain', {
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://136.49.66.51/bird-brain', {
 // mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/bird-brain', {
 
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   family: 4,
+// });
+
+mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   family: 4,
