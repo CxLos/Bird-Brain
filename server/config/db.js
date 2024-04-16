df3b617f587ae4f32f701ae831c1d5d277f17433
cxlos <bautistac_29@outlook.com>
2024-04-15 20:48:13 -0500

    played around with db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-// mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/bird-brain', {
+mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/bird-brain', {
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bird-brain', {
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/bird-brain', {
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/bird-brain', {
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/bird-brain', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  family: 4,
 });
 
 module.exports = mongoose.connection;
