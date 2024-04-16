0d8acac9b29f8e35d9e191c0da042cf70dc7a178
cxlos <bautistac_29@outlook.com>
2024-04-15 23:23:37 -0500

    played with connection string

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,27 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
+const uri = 'mongodb://cxlos:c6uinQzP6UjGcwS2:27017/bird-brain';
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bird-brain', {
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+    autoIndex: false, // Don't build indexes
+    maxPoolSize: 10, // Maintain up to 10 socket connections
+    serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    family: 4 // Use IPv4, skip trying IPv6
+}
+
+const connectWithDB = () => {
+  mongoose.connect(uri, options, (err, db) => {
+    if (err) console.error(err);
+    else console.log("database connection")
+  })
+}
+
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bird-brain', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://cxlos:c6uinQzP6UjGcwS2:27017/bird-brain', {
 // mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/bird-brain', {
 
   useNewUrlParser: true,
@@ -10,3 +30,4 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bird-brai
 });
 
 module.exports = mongoose.connection;
+// module.exports = connectWithDB.connection;
